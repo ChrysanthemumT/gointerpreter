70e74df465d83da407e4de3b72516d1f7ecd35b4
wang <wangminhong73@gmail.com>
2024-03-05 16:01:29 +0800

    Add function call evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -74,6 +74,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
         if len(args) == 1 && isError(args[0]) {
             return args[0]
         }
+        return applyFunction(function, args)
     }
 
     return nil
@@ -251,3 +252,34 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 
     return result
 }
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+    function, ok := fn.(*object.Function)
+    if !ok {
+        return newError("not a function: %s", fn.Type())
+    }
+
+    extendedEnv := extendedFunctionEvn(function, args)
+    evaluated := Eval(function.Body, extendedEnv)
+    return unwrapReturnValue(evaluated)
+}
+
+func extendedFunctionEvn(fn *object.Function, args []object.Object) *object.Environment {
+    env := object.NewEnclosedEnvironment(fn.Env)
+
+    for paramidx, param := range fn.Parameters {
+        env.Set(param.Value, args[paramidx])
+    }
+
+    return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+    if returnValue, ok := obj.(*object.ReturnValue); ok {
+        return returnValue.Value
+    }
+
+    return obj
+}
+
+
