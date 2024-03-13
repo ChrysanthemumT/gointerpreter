e3d6817cabf2b9d6654cb613cccd13f5cae6fe5b
wang <wangminhong73@gmail.com>
2024-03-13 23:26:18 +0800

    Add ArrayIndexing

--- parser/parser.go
+++ parser/parser.go
@@ -33,6 +33,7 @@ const (
     PRODUCT
     PREFIX
     CALL
+    INDEX
 )
 
 var precedences = map[token.TokenType]int{
@@ -45,6 +46,7 @@ var precedences = map[token.TokenType]int{
     token.SLASH: PRODUCT,
     token.ASTERISK: PRODUCT,
     token.LPAREN: CALL,
+    token.LBRACKET: INDEX,
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -75,6 +77,7 @@ func New(l *lexer.Lexer) *Parser {
     p.registerInfix(token.LT, p.parseInfixExpression)
     p.registerInfix(token.GT, p.parseInfixExpression)
     p.registerInfix(token.LPAREN, p.parseCallExpression)
+    p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
     //read two tokens, so curToken and peekToken are both set
     p.nextToken()
@@ -367,6 +370,19 @@ func (p *Parser) parseArrayLiteral() ast.Expression {
     array.Elements = p.parseExpressionList(token.RBRACKET)
 
     return array
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+    exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+    p.nextToken()
+    exp.Index = p.parseExpression(LOWEST)
+
+    if !p.expectPeek(token.RBRACKET) {
+        return nil
+    }
+
+    return exp
 }
 
 func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
